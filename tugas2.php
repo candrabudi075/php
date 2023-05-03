99401cd73edae9ec3a385107005515e4e756360e
candrabudi075 <126483015+candrabudi075@users.noreply.github.com>
2023-05-03 20:57:09 +0700

    Saifullah

---
+++ tugas2.php
@@ -0,0 +1,8 @@
+<?php
+	$nama = array('Nama :pelita','Nama : Najmina','Nama : Anita','Nama : Bayu','Nama : Nasa','Nama : Rahma');
+	
+	
+    for($x=0; $x < count($nama) ; $x++){
+        echo $nama[$x]."<br>";
+    }
+?>
